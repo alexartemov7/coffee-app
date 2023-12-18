8639692698b3ebb62dedfb20d90ec6ea34f73e88
Alex Artemov <artemov.sandro@gmail.com>
2023-12-17 19:40:47 -0500

    Boca Brews

--- src/components/CoffeeList.jsx
+++ src/components/CoffeeList.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useState } from 'react' //imports useState hook from the React library.
+//enables state management, in other words, remembering things
 
 export default function CoffeeList() {
-    const [coffees, setCoffees] = useState()
+    const [coffees, setCoffees] = useState() //initializes state variable named coffees with an associated setter function setCoffees
+    //function getCoffees is defined and used to fetch a list of coffees based on temperature
     const getCoffees = (temperature) => {
         fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-          .then(res => res.json())
-          .then(data => setCoffees(data))
+          .then(res => res.json()) //converts response data into json format
+          .then(data => setCoffees(data)) //sets the coffee state with data received
           .catch(alert)
     }
+    //This return section below starts the section where the component defines what it will visually render.
     return (
-        <main>
+        <main> 
             <div>
                 <button onClick={() => getCoffees('hot')}>Hot</button>
                 <button onClick={() => getCoffees('iced')}>Iced</button>
